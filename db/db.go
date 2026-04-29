ce37ae641e149524e70170d9bc8536accee829a5
iimoh <ivyimoh@gmail.com>
2026-04-29 20:03:49 +0300

    feat:database

---
+++ db/db.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_"github.com/lib/pq"
+)	
+
+var DB *sql.DB
+
+func InitDB() {
+	connStr := "user=postgres password=yourpassword dbname=drugflow sslmode=disable"
+
+	var err error 
+	DB, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("DB connection failed:" err)
+	}
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("DB unreachable:", err)
+
+	}
+	log.Println("DB connection successful")
+}
